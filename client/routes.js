609940e616987d21f963d8faff84c1cd9af44529
hugo-espinelli <hugoespinelli@gmail.com>
2020-07-02 23:04:23 -0300

    adicionando fases relacionadas ao clicar para mover a fases

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,7 @@
 
 const UPDATE_FIELDS_ROUTE = "/pipes/:pipeId/phases/:phaseId/update_fields";
 const MOVE_CARDS_ROUTE = "/pipes/:pipeId/move_cards";
+const PHASES_ROUTE = "/phases/:phaseId";
 
 
-export {UPDATE_FIELDS_ROUTE, MOVE_CARDS_ROUTE};
+export { UPDATE_FIELDS_ROUTE, MOVE_CARDS_ROUTE, PHASES_ROUTE };
