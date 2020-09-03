6ead2d514ee901c8812ea8028204eef220395564
hugo-espinelli <hugoespinelli@gmail.com>
2020-09-02 22:42:29 -0300

    adicionando etiquetagem amarela para novos candidatos

--- back/services/label/consts.js
+++ back/services/label/consts.js
@@ -4,6 +4,7 @@ const LABEL_OPTIONS = {
     POTENCIAL: "Potencial",
     ELIMINADO: "Eliminado",
     CANDIDATO_BASE: "Candidato da Base",
+    NOVO_CANDIDATO: "Novo",
 };
 
 module.exports = { LABEL_OPTIONS };
